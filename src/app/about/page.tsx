485433e672a81939542e2c5bc7311816bd1a182b
WilbertFE <wilbertbernadi699@gmail.com>
2025-08-05 14:01:55 +0700

    feat: about page update

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,7 @@
 import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { ArrowRight } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -8,18 +10,27 @@ import {
   FaGithub,
   FaInstagram,
   FaLinkedin,
-  FaMailchimp,
   FaTiktok,
-  FaVoicemail,
   FaYoutube,
 } from "react-icons/fa6";
-import { SiGmail } from "react-icons/si";
 
 export default function About() {
   return (
     <div className="gap-y-4 flex flex-wrap p-6 container">
       <div className="space-y-2 w-full items-center flex gap-x-16">
-        <div className="min-w-72 min-h-96">
+        <div className="w-1/3">
+          <h1 className="text-4xl font-bold tracking-wider">
+            Hello, I&apos;m
+            <span className="text-cyan-400"> Wilbert Bernardi</span>
+          </h1>
+          <Separator className="min-h-[2px] my-4 bg-yellow-300" />
+          <p className="max-w-full">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis
+            dolor officiis cupiditate. Modi neque mollitia assumenda aliquid ut
+            omnis vero!
+          </p>
+        </div>
+        <div className="w-1/3 min-h-96">
           <AspectRatio className="w-full h-full" ratio={3 / 4}>
             <Image
               src="/img/profile.png"
@@ -29,29 +40,27 @@ export default function About() {
             />
           </AspectRatio>
         </div>
-        <div className="space-y-4 grow">
-          <h1 className="text-4xl font-bold tracking-wider">ABOUT ME</h1>
-          <p className="font-bold text-lg text-cyan-400">
-            Wilbert Bernardi - Web Developer
-          </p>
+        <div className="space-y-4 w-1/3">
+          <p className="font-bold text-lg text-cyan-400">Web Developer</p>
           <p className="text-lg">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident
-            obcaecati pariatur ad explicabo nisi laudantium cumque vitae ullam,
-            voluptate possimus nihil! Ratione quidem at tempore iure eos
-            voluptatum, eius qui hic cum. Illo optio, ut doloribus esse
-            recusandae id dolorem, asperiores ducimus eum maiores debitis
-            consequuntur sapiente similique, magni minima.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur,
+            omnis.
           </p>
+          <div className="flex gap-x-2 text-yellow-300">
+            <span>show more</span>
+            <ArrowRight />
+          </div>
           <div className="flex gap-x-8 mb-8">
             {sosmed.map((item, i) => (
-              <Link key={i} href={item.href}>
-                <item.icon size={32} />
+              <Link
+                key={i}
+                href={item.href}
+                className="bg-foreground p-2 rounded-full"
+              >
+                <item.icon className="text-yellow-600" size={32} />
               </Link>
             ))}
           </div>
-          <Button className="text-lg p-6" asChild variant="outline">
-            <Link href="/">More info</Link>
-          </Button>
         </div>
       </div>
     </div>
