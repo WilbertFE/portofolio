c3af2493f86d48f502f269208814515e2021a79b
WilbertFE <wilbertbernadi699@gmail.com>
2025-08-30 16:40:33 +0700

    feat: complete project component

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,8 +1,9 @@
+"use client";
+
 import React from "react";
 import {
   Card,
   CardAction,
-  CardContent,
   CardDescription,
   CardFooter,
   CardHeader,
@@ -10,13 +11,25 @@ import {
 } from "@/components/ui/card";
 import Image from "next/image";
 import { IconType } from "react-icons";
+import { DiCss3, DiHtml5 } from "react-icons/di";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { RiJavascriptFill, RiTailwindCssFill } from "react-icons/ri";
+import { FaLocationArrow } from "react-icons/fa6";
+import { TiLocationArrow } from "react-icons/ti";
+
+type Icon = {
+  icon: IconType;
+  color: string;
+};
 
 type Projects = {
   title: string;
   description: string;
+  image: string;
   href: string;
   year: number;
-  icons: IconType[];
+  icons: Icon[];
 };
 
 export default function page() {
@@ -24,62 +37,168 @@ export default function page() {
     <div className="flex flex-wrap gap-y-16 px-8 pb-32 pt-16 container mx-auto">
       <div className="w-full space-y-8">
         <div>
-          <h1>Personal Projects</h1>
-          <p>Proyek personal saya.</p>
-        </div>
-        <div className="grid grid-cols-2 gap-8">
-          <Card className="p-1">
-            <div className="max-w-full h-64 relative">
-              <Image
-                src="/img/landing-page-website.jpg"
-                alt="landing page"
-                fill
-                className="object-cover object-center rounded-sm"
-              />
-            </div>
-            <CardHeader>
-              <CardTitle>Card Title</CardTitle>
-              <CardDescription>Card Description</CardDescription>
-              {/* <CardAction>Card Action</CardAction> */}
-            </CardHeader>
-            {/* <CardContent>
-              <p>Card Content</p>
-            </CardContent> */}
-            <CardFooter>
-              <p>Card Footer</p>
-            </CardFooter>
-          </Card>
-        </div>
-      </div>
-      <div className="w-full space-y-8">
-        <div>
           <h1>Learning Projects</h1>
           <p>Proyek yang saya buat untuk belajar.</p>
         </div>
         <div className="grid grid-cols-2 gap-8">
-          <Card className="p-1">
-            <div className="max-w-full h-64 relative">
-              <Image
-                src="/img/landing-page-website.jpg"
-                alt="landing page"
-                fill
-                className="object-cover object-center rounded-sm"
-              />
+          {learningProjects.map((project, i) => (
+            <div key={i} className="border bg-transparent p-1 rounded-xl">
+              <Card className="px-4 py-6 h-full">
+                <div className="max-w-full h-64 relative">
+                  <Image
+                    src={project.image}
+                    alt={project.title}
+                    fill
+                    className="object-cover object-center rounded-sm"
+                  />
+                </div>
+                <CardHeader className="space-x-4">
+                  <CardTitle className="text-lg tracking-wider">
+                    {project.title}{" "}
+                    <span className="text-muted-foreground text-base">
+                      {"["}
+                      {project.year}
+                      {"]"}
+                    </span>
+                  </CardTitle>
+                  <CardDescription>{project.description}</CardDescription>
+                  <CardAction className="self-center">
+                    <Button
+                      asChild
+                      variant="link"
+                      className="tracking-wider font-bold text-my-primary"
+                    >
+                      <Link className="relative" href={project.href}>
+                        Visit{" "}
+                        <TiLocationArrow className="absolute top-0 -right-1" />
+                      </Link>
+                    </Button>
+                  </CardAction>
+                </CardHeader>
+                <CardFooter className="space-x-4">
+                  {project.icons.map((icon, i) => (
+                    <icon.icon key={i} size={32} color={icon.color} />
+                  ))}
+                </CardFooter>
+              </Card>
             </div>
-            <CardHeader>
-              <CardTitle>Card Title</CardTitle>
-              <CardDescription>Card Description</CardDescription>
-              {/* <CardAction>Card Action</CardAction> */}
-            </CardHeader>
-            {/* <CardContent>
-              <p>Card Content</p>
-            </CardContent> */}
-            <CardFooter>
-              <p>Card Footer</p>
-            </CardFooter>
-          </Card>
+          ))}
         </div>
       </div>
     </div>
   );
 }
+
+const learningProjects: Projects[] = [
+  {
+    title: "Calculator",
+    description:
+      "Dapat menyelesaikan operasi aritmatik sederhana seperti tambah, kurang, bagi, kali.",
+    image: "/img/projects/learning/calculator.png",
+    href: "https://wilbertfe.github.io/Calculator/",
+    year: 2023,
+    icons: [
+      {
+        icon: DiHtml5,
+        color: "red",
+      },
+      {
+        icon: DiCss3,
+        color: "lightblue",
+      },
+      { icon: RiJavascriptFill, color: "yellow" },
+    ],
+  },
+  {
+    title: "Weather Forecast",
+    description:
+      "Aplikasi yang dapat memprediksi cuaca saat ini dan meramal cuaca di masa depan. Terkoneksi dengan API OpenWeatherMap.",
+    image: "/img/projects/learning/weatherapp.png",
+    href: "https://wilbertfe.github.io/weatherapp/",
+    year: 2023,
+    icons: [
+      {
+        icon: DiHtml5,
+        color: "red",
+      },
+      {
+        icon: DiCss3,
+        color: "lightblue",
+      },
+      { icon: RiJavascriptFill, color: "yellow" },
+    ],
+  },
+  {
+    title: "Portofolio V1",
+    description: "Website portofolio pertama saya.",
+    image: "/img/projects/learning/portofolio1.png",
+    href: "https://wilbertfe.github.io/PortofolioV1/",
+    year: 2023,
+    icons: [
+      {
+        icon: DiHtml5,
+        color: "red",
+      },
+      {
+        icon: DiCss3,
+        color: "lightblue",
+      },
+      { icon: RiJavascriptFill, color: "yellow" },
+    ],
+  },
+  {
+    title: "wBurger",
+    description: "Website yang saya buat untuk belajar layouting.",
+    image: "/img/projects/learning/burger-web.png",
+    href: "https://wilbertfe.github.io/burgerWeb/",
+    year: 2023,
+    icons: [
+      {
+        icon: DiHtml5,
+        color: "red",
+      },
+      {
+        icon: DiCss3,
+        color: "lightblue",
+      },
+      { icon: RiJavascriptFill, color: "yellow" },
+    ],
+  },
+  {
+    title: "Jaaz",
+    description: "Website yang saya buat untuk belajar layouting.",
+    image: "/img/projects/learning/Jaaz.png",
+    href: "https://wilbertfe.github.io/Jaaz/",
+    year: 2023,
+    icons: [
+      {
+        icon: DiHtml5,
+        color: "red",
+      },
+      {
+        icon: RiTailwindCssFill,
+        color: "cyan",
+      },
+      { icon: RiJavascriptFill, color: "yellow" },
+    ],
+  },
+  {
+    title: "Github Search",
+    description:
+      "Terhubung dengan API Github. Aplikasi untuk Menemukan profile user github dan repository.",
+    image: "/img/projects/learning/github-search.png",
+    href: "https://wilbertfe.github.io/Jaaz/",
+    year: 2023,
+    icons: [
+      {
+        icon: DiHtml5,
+        color: "red",
+      },
+      {
+        icon: RiTailwindCssFill,
+        color: "cyan",
+      },
+      { icon: RiJavascriptFill, color: "yellow" },
+    ],
+  },
+];
