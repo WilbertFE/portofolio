400b3cd3e8bb948a59042061b544069ead3b1429
WilbertFE <wilbertbernadi699@gmail.com>
2025-10-03 12:25:52 +0700

    feat: add form section in contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,104 +1,109 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
 import { Separator } from "@/components/ui/separator";
-import { Header } from "./components";
+import { Header, Info } from "./components";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
-import { Mail } from "lucide-react";
+import { Textarea } from "@/components/ui/textarea";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-export default function page() {
+const formSchema = z.object({
+  name: z.string().min(3).max(50),
+  email: z.email(),
+  message: z.string().min(3).max(256),
+});
+
+export default function ContacePage() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      message: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+
   return (
     <div className="p-6 space-y-4 mx-auto h-full container">
       <Header />
       <Separator className="mb-12" />
-      <div className="grid grid-cols-2 gap-x-6 gap-y-6">
-        <div className="shadow p-0.5 shadow-neutral-600 col-span-2">
-          <div className="p-4 col-span-2 flex gap-x-12 bg-neutral-900 ">
-            <div className="space-y-4 grow">
-              <h1 className="text-xl font-bold tracking-wide">
-                Stay in touch!
-              </h1>
-              <p className="text-muted-foreground">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,
-                fugiat.
-              </p>
-              <Button>See in Gmail</Button>
-            </div>
-            <div className="flex rounded-md shadow p-0.5 shadow-neutral-600 border self-end">
-              <Mail size={64} />
-            </div>
-          </div>
-        </div>
-        <div className="shadow p-0.5 shadow-neutral-600">
-          <div className="p-4 col-span-2 flex gap-x-12 bg-neutral-900 ">
-            <div className="space-y-4 grow">
-              <h1 className="text-xl font-bold tracking-wide">
-                Stay in touch!
-              </h1>
-              <p className="text-muted-foreground">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,
-                fugiat.
-              </p>
-              <Button>See in Gmail</Button>
-            </div>
-            <div className="flex rounded-md shadow p-0.5 shadow-neutral-600 border self-end">
-              <Mail size={64} />
-            </div>
-          </div>
-        </div>
-        <div className="shadow p-0.5 shadow-neutral-600">
-          <div className="p-4 col-span-2 flex gap-x-12 bg-neutral-900 ">
-            <div className="space-y-4 grow">
-              <h1 className="text-xl font-bold tracking-wide">
-                Stay in touch!
-              </h1>
-              <p className="text-muted-foreground">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,
-                fugiat.
-              </p>
-              <Button>See in Gmail</Button>
-            </div>
-            <div className="flex rounded-md shadow p-0.5 shadow-neutral-600 border self-end">
-              <Mail size={64} />
-            </div>
-          </div>
-        </div>
-        <div className="shadow p-0.5 shadow-neutral-600">
-          <div className="p-4 col-span-2 flex gap-x-12 bg-neutral-900 ">
-            <div className="space-y-4 grow">
-              <h1 className="text-xl font-bold tracking-wide">
-                Stay in touch!
-              </h1>
-              <p className="text-muted-foreground">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,
-                fugiat.
-              </p>
-              <Button>See in Gmail</Button>
-            </div>
-            <div className="flex rounded-md shadow p-0.5 shadow-neutral-600 border self-end">
-              <Mail size={64} />
-            </div>
-          </div>
-        </div>
-        <div className="shadow p-0.5 shadow-neutral-600">
-          <div className="p-4 col-span-2 flex gap-x-12 bg-neutral-900 ">
-            <div className="space-y-4 grow">
-              <h1 className="text-xl font-bold tracking-wide">
-                Stay in touch!
-              </h1>
-              <p className="text-muted-foreground">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,
-                fugiat.
-              </p>
-              <Button>See in Gmail</Button>
-            </div>
-            <div className="flex rounded-md shadow p-0.5 shadow-neutral-600 border self-end">
-              <Mail size={64} />
-            </div>
-          </div>
-        </div>
-      </div>
+      <Info />
       <Separator />
-      <div>
-        <p>or send me a message</p>
-      </div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <div className="grid grid-cols-2 gap-4">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Input your name" {...field} />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Input your email" {...field} />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="message"
+              render={({ field }) => (
+                <FormItem className="col-span-2">
+                  <FormLabel>Message</FormLabel>
+                  <FormControl>
+                    <Textarea placeholder="Input your message" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="col-span-2">
+              <Button className="w-full cursor-pointer" type="submit">
+                Send Email
+              </Button>
+            </div>
+          </div>
+        </form>
+      </Form>
     </div>
   );
 }
