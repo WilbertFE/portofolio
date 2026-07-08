f67df92526a26e5f821fcbeca206a2b8f6c7698f
WilbertFE <wilbertbernadi699@gmail.com>
2026-07-08 17:08:44 +0700

    feat: add metadata og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,24 @@ const spaceGrotesk = Space_Grotesk({
 });
 
 export const metadata: Metadata = {
-  title: "Portofolio",
+  title: "Wilbert Bernardi - Web Developer Portfolio",
   description: "Website portofolio Wilbert Bernardi",
+  openGraph: {
+    title: "Wilbert Bernardi - Web Developer Portfolio",
+    description: "Explore my projects and tech stack",
+    url: "https://wilbertbernardi.vercel.app",
+    siteName: "Wilbert Bernardi Portfolio",
+    images: [
+      {
+        url: "https://wilbertbernardi.vercel.app/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Wilbert Bernardi Portfolio",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
